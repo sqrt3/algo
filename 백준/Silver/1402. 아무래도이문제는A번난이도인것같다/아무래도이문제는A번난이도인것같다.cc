22226c47c67c872ea3c65877724f1802ec8bae49
sqrt3 <1dimchae@gmail.com>
2024-08-26 10:05:11 +0900

    [Silver V] Title: 아무래도이문제는A번난이도인것같다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1402. 아무래도이문제는A번난이도인것같다/아무래도이문제는A번난이도인것같다.cc
@@ -0,0 +1,16 @@
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	int cnt;
+    cin >> cnt;
+	long long x, y;
+	for (int i = 0; i < cnt; i++) {
+		cin >> x >> y;
+		cout << "yes\n";
+    }
+	return 0;
+}
