486b127fdfb69bde8f74766252434a29f25e8ae5
sqrt3 <1dimchae@gmail.com>
2024-10-07 16:38:03 +0900

    [Gold IV] Title: 좋다, Time: 16 ms, Memory: 2160 KB -BaekjoonHub

---
+++ 백준/Gold/1253. 좋다/좋다.cc
@@ -0,0 +1,49 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+    vector<int> m(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> m[i];
+    }
+
+    sort(m.begin(), m.end());
+    int cnt = 0;
+    unordered_set<int> found;
+
+    for (int i = 0; i < n; i++) {
+        int b = 0, e = n - 1;
+        while (b < e) {
+            if (b == i) { b++; continue; }
+            if (e == i) { e--; continue; }
+
+            int sum = m[b] + m[e];
+            if (sum == m[i]) {
+                cnt++;
+                found.insert(m[i]);
+                break;
+            }
+
+            if (sum < m[i]) {
+                b++;
+            }
+            else {
+                e--;
+            }
+        }
+    }
+
+    cout << cnt;
+
+    return 0;
+}
